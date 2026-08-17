dccb0fc58256158586baef9b4f6c7ec1c379e1b8
Azizbek <156009541+azizbekjumaboyev9111@users.noreply.github.com>
2026-08-17 12:05:30 +0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,5 +2,9 @@ const PDF_FILES = [
   {
     id: "6055c9c9-e746-4b59-8997-61bc47f73000",
     fileUrl: "files/demopdf.pdf"
+  },
+  {
+    id: "0ad2852b-1e11-414a-909b-4cb2c18ac127",
+    fileUrl: "files/demopdf.pdf"
   }
 ];
