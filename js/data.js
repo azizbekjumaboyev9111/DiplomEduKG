d9f91778de64dd0bf6b3b7ce66796aaa3b3a03ff
Azizbek <156009541+azizbekjumaboyev9111@users.noreply.github.com>
2026-08-17 20:53:47 +0500

    fix

--- js/data.js
+++ js/data.js
@@ -6,5 +6,9 @@ const PDF_FILES = [
   {
     id: "0ad2852b-1e11-414a-909b-4cb2c18ac127",
     fileUrl: "files/diplom.pdf"
+  },
+  {
+    id: "61366c57-ed51-4c28-a7d4-0840661cae33",
+    fileUrl: "files/demov2.pdf"
   }
 ];
